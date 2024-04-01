f1de4f1834f67388f69db4e404897e4bc4642941
Wojtek Nowak <billnewmann@gmail.com>
2024-04-01 23:03:14 +0100

    save buttons as variables

--- src/js/app.js
+++ src/js/app.js
@@ -19,6 +19,18 @@ let databaseCultural = 100;
 
 let eventCounter = 0;
 
+const cargoColonistsButton = document.getElementById("cargoColonists");
+const cargoProbesButton = document.getElementById("cargoProbes");
+const scannerAtmosphereButton = document.getElementById("scannerAtmosphere");
+const scannerGravityButton = document.getElementById("scannerGravity");
+const scannerTemperatureButton = document.getElementById("scannerTemperature");
+const scannerWaterButton = document.getElementById("scannerWater");
+const scannerResourcesButton = document.getElementById("scannerResources");
+const systemLandingButton = document.getElementById("systemLanding");
+const systemConstructionButton = document.getElementById("systemConstruction");
+const databaseScientificButton = document.getElementById("databaseScientific");
+const databaseCulturalButton = document.getElementById("databaseCultural");
+
 const events = {
   name: "PROTOPLANETARY DISC",
   description: `The seedship's course takes it close to a newly-formed star that is still surrounded by a protoplanetary disc: a whirling chaos of incandescent gas and molten rocks that the young star's gravity has gathered from the star-forming cloud, and which is now undergoing the countless violent collisions that will eventually form it into a planetary system. There can be no home for humanity here, but passing through the disc would give the AI enough data about planet formation to upgrade one of its scanners. It is a dangerous region, however, and passing through would risk collision with a planetesimal.`,
