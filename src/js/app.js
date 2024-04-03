8d5ac5af37814e567062c1f42c3c7ae0c63823bc
Wojtek Nowak <billnewmann@gmail.com>
2024-04-03 23:33:35 +0100

    add await button spinner

--- src/js/app.js
+++ src/js/app.js
@@ -42,6 +42,26 @@ const hibernateButton = document.getElementById(`hibernateButton`);
 const shutdownButton = document.getElementById(`shutdownButton`);
 const terminateButton = document.getElementById(`terminateButton`);
 
+//await button spinner
+
+awaitButton.innerHTML = "AWAIT (/)";
+
+const awaitSpinner = function (current) {
+  if (current == "AWAIT (/)") {
+    return "AWAIT (-)";
+  } else if (current == "AWAIT (-)") {
+    return "AWAIT (\\)";
+  } else if (current == "AWAIT (\\)") {
+    return "AWAIT (|)";
+  } else if (current == "AWAIT (|)") {
+    return "AWAIT (/)";
+  }
+};
+
+setInterval(() => {
+  awaitButton.innerHTML = awaitSpinner(awaitButton.innerHTML);
+}, 300);
+
 const events = {
   name: "PROTOPLANETARY DISC",
   description: `The seedship's course takes it close to a newly-formed star that is still surrounded by a protoplanetary disc: a whirling chaos of incandescent gas and molten rocks that the young star's gravity has gathered from the star-forming cloud, and which is now undergoing the countless violent collisions that will eventually form it into a planetary system. There can be no home for humanity here, but passing through the disc would give the AI enough data about planet formation to upgrade one of its scanners. It is a dangerous region, however, and passing through would risk collision with a planetesimal.`,
