77d6ac6016f6cf87e5c5bf6acbff59ef8b2e1632
Wojtek Nowak <billnewmann@gmail.com>
2024-04-04 22:53:01 +0100

    work on events database

--- src/js/app.js
+++ src/js/app.js
@@ -65,4 +65,16 @@ setInterval(() => {
 const events = {
   name: "PROTOPLANETARY DISC",
   description: `The seedship's course takes it close to a newly-formed star that is still surrounded by a protoplanetary disc: a whirling chaos of incandescent gas and molten rocks that the young star's gravity has gathered from the star-forming cloud, and which is now undergoing the countless violent collisions that will eventually form it into a planetary system. There can be no home for humanity here, but passing through the disc would give the AI enough data about planet formation to upgrade one of its scanners. It is a dangerous region, however, and passing through would risk collision with a planetesimal.`,
+  option1: {
+    button: `Stay on course`,
+    outcome: {
+      outcomeText: `Seedship continues its journey leaving the disc behind forever`,
+    },
+  },
+  option2: {
+    button: `Pass through disc`,
+    outcome: {
+      outcomeText: `The seedship observes birth of the planet, and passes through the disc safely. Scientific data gathered allows its systems to improve`,
+    },
+  },
 };
